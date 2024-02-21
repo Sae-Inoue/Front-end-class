dddf655a548d6ca9754a7303a8503594d905167d
Sae Inoue <20103543@tafe.wa.edu.au>
2024-02-21 12:25:29 +0800

    exercise 04-01 finish

--- 04-SaaS-Exercises/exercise-01.php
+++ 04-SaaS-Exercises/exercise-01.php
@@ -4,43 +4,37 @@
  *
  * Problem Description
  *
- * Author:      Student Name <student email>
- * Version:     0.0
+ * Author:      Sae Inoue <20103543@tafe.wa.edu.au>
+ * Version:     1.0
  *
- * Within this file you will find a number of TO DO statements
- * that will describe what needs to be completed in this exercise.
  *
  * Follow the instructions carefully.
  *
  */
 
-// TODO:  1 Create a variable called phonetic.
-// TODO:  2 Add the Radio Phonetic alphabet to this variable as an
-//        array of values
 
-// TODO:  3 Create a variable called symbols.
-// TODO:  4 Add the symbols !@#$%^&<>? and a space as an array of values
+$phonetic = ['Alfa','Bravo','Charlie','Delta','Echo','Foxtrot'];
 
-// TODO:  5 Create a new variable called password, and make sure it is empty
+$symbols = ['!','@','#','$','%','^','&','<','>','?',' '];
 
-// TODO:  6 Create two new variables, word1 and word2 and make them empty
-// TODO:  7 Create two more variables, symbol1 and symbol2 and make them empty
+$password = ' ';
 
-// TODO:  8 Use the mt_rand function to select a word from the phonetic
-//          array, and store in word1
+$word1 =' ';
+$word2 =' ';
+$symbol1 =' ';
+$symbol2 =' ';
 
-// TODO:  9 Use the mt_rand function to select a word from the phonetic
-//          array, and store in word2
 
-// TODO: 10 Use the mt_rand function to select a symbol from the symbols array,
-//          and store in symbol1
 
-// TODO: 11 Use the mt_rand function to select a word from the symbols array, and
-//          store in symbol2
+$word1 = $phonetic[mt_rand(0,count($phonetic)-1)];
 
-// TODO: 12 Use these four parts to create a new password in the form:
-//          symbol1 word1 word2 symbol2
+$word2 = $phonetic[mt_rand(0,count($phonetic)-1)];
 
+$symbol1 = $symbols[mt_rand(0,count($symbols)-1)];
+
+$symbol2 = $symbols[mt_rand(0,count($symbols)-1)];
+
+$password = $symbol1.$word1.$word2.$symbol2;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,29 +42,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- TODO: 13 make the title: Session 04 -->
-    <title>TITLE GOES HERE</title>
+
+    <title>Session 04</title>
 </head>
 
 <body class="bg-gray-100 flex flex-col min-h-screen">
 <header class="bg-gray-800 text-white p-4">
     <div class="container mx-auto flex flex-row justify-between">
-        <h1 class="text-3xl font-semibold">YOUR NAME GOES HERE</h1>
-        <!-- TODO: 14 make the Topic "Exercise 01" -->
-        <h2 class="text-2xl">TOPIC: Subtopic</h2>
+        <h1 class="text-3xl font-semibold">Sae Inoue</h1>
+
+        <h2 class="text-2xl">TOPIC: Exercise 01</h2>
     </div>
 </header>
 <main class="container mx-auto p-4 mt-4 gap-4 flex flex-col">
     <article class="bg-white rounded-lg shadow-md shadow-gray-500/20 p-6">
         <header>
-        <!-- TODO: 15 make the heading "Password Maker" -->
-            <h2 class="text-2xl font-semibold mb-4">OUTPUT HEADING</h2>
+
+            <h2 class="text-2xl font-semibold mb-4">Password Maker</h2>
         </header>
         <section>
             <?php
-            // TODO: Using a suitable method...
-            // TODO: 16 Output the password in a <p>aragraph</p>
-            // TODO: 17 Add the class 'text-xl' to the pargraph
+
+            echo "<p class='text-xl'>$password</p>";
 
             ?>
         </section>
@@ -81,7 +74,7 @@
     <section class="container mx-auto flex flex-row flex-wrap justify-between">
         <div class="w-1/2">
             <p>
-                &copy; Copyright 2024 YOUR NAME HERE, All rights Reserved
+                &copy; Copyright 2024 Sae Inoue, All rights Reserved
             </p>
         </div>
         <div class="w-1/2">
@@ -94,13 +87,5 @@
         </div>
     </section>
 </body>
-<!-- TODO: 18 Test your solution. -->
-<!-- TODO: 19 Resolve any errors, if they exist. -->
-<!-- TODO: 20 Remove the comment from the top of the document that starts
-            * Within this file you ...
-           and ends with
-            * and save your work.
--->
-<!-- TODO: 21 Update the version to 1.0 -->
-<!-- TODO: Finally remove all TODO comments from the code. -->
+
 </html>
