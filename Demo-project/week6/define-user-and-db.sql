c516b155ff12d1b2592cc877c6adb16ec1f02220
Sae Inoue <20103543@tafe.wa.edu.au>
2024-03-06 11:03:01 +0800

    start week6

---
+++ Demo-project/week6/define-user-and-db.sql
@@ -0,0 +1,8 @@
+-- Define Database (INITIALS_saas_fed)
+
+CREATE DATABASE ajg_saas_fed;
+
+-- Define Database User & Give access to Database
+-- Username: INITIALS_saas_fed;
+-- Password: Password1
+-- Host: 127.0.0.1 (localhost can cause issues due to IP)
